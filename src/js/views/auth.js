759a346baf357a3208a6ff154f134ebec37cc420
Alejandro Sanchez <aalejo@gmail.com>
2020-07-03 16:18:42 +0000

    no more replits

--- src/js/views/auth.js
+++ src/js/views/auth.js
@@ -6,7 +6,7 @@ export const LoginView = propers => (
 	<Login
 		onSubmit={(username, password) => login(username, password, propers.history)}
 		logoURL={process.env.LOGO_URL}
-		appName={process.env.APP_NAME}
+		appName={"Teacher"}
 		onForgot={() => propers.history.push("/forgot")}
 	/>
 );
@@ -14,7 +14,7 @@ export const ForgotView = propers => (
 	<Forgot
 		onSubmit={username => remind(username)}
 		logoURL={process.env.LOGO_URL}
-		appName={process.env.APP_NAME}
+		appName={"Teacher"}
 		onBackToLogin={() => propers.history.push("/login")}
 	/>
 );
