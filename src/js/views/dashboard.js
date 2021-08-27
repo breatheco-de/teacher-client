31b22a183c649d7a31bb0003d028b12a9adce8fb
jefer94 <jdefreitaspinto@gmail.com>
2021-08-27 05:57:51 -0500

    fix syllabus routes

--- src/js/views/dashboard.js
+++ src/js/views/dashboard.js
@@ -181,7 +181,9 @@ class AttendancyView extends React.Component {
 								return (
 									<li key={i}>
 										<CheckBox
-											label={`${s.user.first_name} ${s.user.last_name ? s.user.last_name : ""}`}
+											label={`${s.profile_academy.first_name} ${
+												s.profile_academy.last_name ? s.profile_academy.last_name : ""
+											}`}
 											checked={checked}
 											onClick={isCheck => this.setState({ rsvp: isCheck ? rsvp.concat(s) : rsvp })}
 										/>
